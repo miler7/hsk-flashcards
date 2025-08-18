681e79388d18c53d3f9ca6b7b14e89c4041ae9bf
Justin Miller <44529625+miler7@users.noreply.github.com>
2025-08-18 20:18:52 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME='flashcards-preloaded-v23';
+
+const CACHE_NAME='flashcards-preloaded-v24';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-512.png','./sw.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
